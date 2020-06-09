82a591ea25055db798387c2a0b97de2a0fc14eff
Nobi <nobi@nobidev.com>
2020-06-09 20:53:27 +0700

    fixup! A

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-find pkgs -name "*.deb" -exec dpkg -i {} +
-find pkgs -name "*.deb" -exec dpkg -i {} +
+find packages -name "*.deb" -exec dpkg -i {} +
 find wheels -name "*.whl" -exec pip install {} \;
